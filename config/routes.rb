95252f00d28cd1344ab97387457500cc286ecef3
kubota <kubota@lanches.co.jp>
2013-07-24 00:05:11 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "User Pages" do
-	subject {page}
+	subject { page }
 
   describe "signup page" do
   	before {visit signup_path}
-  	it { should have_selector('h1', text:'Sign up')}
-  	it { should have_selector('title', text: full_title('Sign up'))}
+  	it { should have_selector('h1', text:'Sign up') }
+  	it { should have_selector('title', text: full_title('Sign up')) }
   end
 
   describe "profile page" do
@@ -14,7 +14,7 @@ describe "User Pages" do
   	before { visit user_path(user) }
 
   	it { should have_selector('h1', text: user.name) }
-  	it { should have_selector('title', text: user.name)}
+  	it { should have_selector('title', text: user.name) }
   end
 
   describe "signup" do
@@ -26,6 +26,13 @@ describe "User Pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text:'Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -40,6 +47,16 @@ describe "User Pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text:'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
     end
   end
 end
